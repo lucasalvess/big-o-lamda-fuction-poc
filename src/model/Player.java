5e3ec93ab5c73321ece8ff1cca2360c4057ce1bf
lucasalvess <lcluphus@gmail.com>
2021-10-02 20:53:26 -0300

    Adding model Team + increment Video Player internal implementation

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,17 +5,21 @@ import java.util.List;
 public class Player {
 
     private String name;
+    private Integer level;
+    private List<Video> lastMatches;
 
-    public Player(String name, List<Video> lastMatches) {
+    public Player(String name, Integer level, List<Video> lastMatches) {
         this.name = name;
+        this.level = level;
         this.lastMatches = lastMatches;
     }
 
-    private List<Video> lastMatches;
-
-
     public String getName() {
         return name;
+    }
+
+    public Integer getLevel() {
+        return level;
     }
 
     public List<Video> getLastMatches() {
--- src/model/Video.java
+++ src/model/Video.java
@@ -4,8 +4,13 @@ public class Video {
 
     private String s3FileUrl;
 
-    public void watch(Player player) throws InterruptedException {
-        Thread.sleep(50);
-        System.out.println("Watching match for player " + player.getName());
+    public void watch(Player player) {
+        try {
+            Thread.sleep(50);
+            System.out.println("Watching match for player " + player.getName());
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+
     }
 }
